51f13d787dfdf2c2cd73434c21fb78aff4966b97
Qing1983 <27447985@qq.com>
2018-05-02 11:51:15 +0800

    添加渲染输出的代码

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -16,18 +16,23 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
+		// 加载数据库驱动
 		Class.forName("org.postgresql.Driver");
 		Class.forName("com.mysql.jdbc.Driver");
 
+		// 读取配置文件
 		if (!Config.init()) {
 			log.error("Config init failed.");
 			return;
 		}
+		
+		// 填充数据库表VO
 		Connection conn = DriverManager.getConnection(global.Config.mysqlUrl, global.Config.mysqlUser, global.Config.mysqlPassword);
-
 		DBVo dbVo = new DBVo();
 		dbVo.load(conn);
 		conn.close();
+		
+		// 渲染输出
 		BeetlRenderUtil.render(dbVo);
 	}
 }
