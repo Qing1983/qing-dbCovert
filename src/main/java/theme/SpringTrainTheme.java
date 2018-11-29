9385df8c5a45938fcd11e0660562c715da9e8d96
Qing1983 <27447985@qq.com>
2018-11-29 17:52:12 +0800

    加入mapper

--- src/main/java/theme/SpringTrainTheme.java
+++ src/main/java/theme/SpringTrainTheme.java
@@ -46,8 +46,11 @@ public class SpringTrainTheme {
 	String controllerPackage = "controller.${ctx.moduleName}"; // controller包名
 	String controllerClass = "${ctx.tableVo.camelTableName}Controller"; // controller类名
 	String controllerObj = ""; // controller对象名
-	// String mapperPackage = "${ctx.moduleName}"; // mapper包名
-	// String mapperClass = "${tableVo.camelTableName}Controller"; // mapper类名
+	
+	String mapperxmlPackage = "${ctx.moduleName}"; // mapper包名
+	String mapperxmlClass = "${ctx.tableVo.camelTableName}Mapper"; // mapper类名
+	String mapperxmlObj="";
+	
 
 	// ========================================================================
 	// 定义输出文件夹和模版文件夹
@@ -65,6 +68,10 @@ public class SpringTrainTheme {
 
 	}
 
+	/**
+	 * 渲染名称
+	 * @throws IOException
+	 */
 	public void renderName() throws IOException {
 		daoPackage = prefixPackage + BeetlRenderUtil.renderText("ctx", (Object) this, daoPackage);
 		daoClass = StringTool.upperCaseFirst(BeetlRenderUtil.renderText("ctx", (Object) this, daoClass));
@@ -87,31 +94,49 @@ public class SpringTrainTheme {
 		controllerPackage = prefixPackage + BeetlRenderUtil.renderText("ctx", (Object) this, controllerPackage);
 		controllerClass = StringTool.upperCaseFirst(BeetlRenderUtil.renderText("ctx", (Object) this, controllerClass));
 		controllerObj = StringTool.lowerCaseFirst(controllerClass);
+		
+		mapperxmlPackage = "mapper." + BeetlRenderUtil.renderText("ctx", (Object) this, mapperxmlPackage);
+		mapperxmlClass = StringTool.upperCaseFirst(BeetlRenderUtil.renderText("ctx", (Object) this, mapperxmlClass));
+		mapperxmlObj=StringTool.lowerCaseFirst(mapperxmlClass);
 
 		log.info(JsonTool.toJSON(this));
 	}
 
-	public void renderFile(String packageName, String className, String objName, String templateName) throws Exception {
-		String outFilePath = outPath + "/" + packageName.replace('.', '/') + "/" + className + ".java";
+	/**
+	 * 渲染文件
+	 * @param packageName 包名
+	 * @param className 类名
+	 * @param objName 对象名
+	 * @param fileSuffix 生成文件的后缀名
+	 * @param templateName 模版名称
+	 * @throws Exception
+	 */
+	public void renderFile(String packageName, String className, String objName, String fileSuffix,  String templateName) throws Exception {
+		
+		// 定义输出对应的文件
+		String outFilePath = outPath + "/" + packageName.replace('.', '/') + "/" + className + fileSuffix;
 
 		// 根据模版渲染文件
 		Template template = BeetlRenderUtil.getFileTemplate(beetlPath,   templateName );
 		template.binding("ctx", this);
 		String text = template.render();
 		log.info("\n"+ text);
-		// 写入文件
+		
+		// 渲染的内容写入文件
 		StringTool.writeStringToFile(outFilePath, text);
-
 	}
 
+	/**
+	 * 渲染主题涉及的各个文件
+	 * @throws Exception
+	 */
 	public void render() throws Exception {
 		renderName();
-		renderFile(controllerPackage, controllerClass, controllerObj, "controller.btl");
-		renderFile(daoPackage, daoClass, daoObj, "dao.btl");
-		renderFile(modelPackage, modelClass, modelObj, "model.btl");
-		renderFile(servicePackage, serviceClass, serviceObj, "service.btl");
-		renderFile(implPackage, implClass, implObj, "serviceImpl.btl");
-		
+		renderFile(controllerPackage, controllerClass, controllerObj, ".java", "controller.btl");
+		renderFile(daoPackage, daoClass, daoObj, ".java", "dao.btl");
+		renderFile(modelPackage, modelClass, modelObj, ".java", "model.btl");
+		renderFile(servicePackage, serviceClass, serviceObj, ".java", "service.btl");
+		renderFile(implPackage, implClass, implObj, ".java", "serviceImpl.btl");
+		renderFile(mapperxmlPackage, mapperxmlClass, mapperxmlObj, ".xml", "mapper.btl");	
 	}
-
 }
